98fd34fd3a524a26ebb5f6a18d448f56b7587aca
qwehash510 <qwehash510@gmail.com>
2026-03-28 01:56:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ client.flood_sleep_threshold = 0
 logging.basicConfig(level=logging.ERROR)
 ban_active = False
 
-@client.on(events.NewMessage(pattern='/x', chats=None))
+@client.on(events.NewMessage(pattern='/.', chats=None))
 async def god_mode_ban(event):
     if event.out:
         return
